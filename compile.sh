cbc7fd39c330f0754edafd079de8544838079f0b
Pedro Ferreira <pedrotadim@outlook.com>
2018-04-26 13:07:58 +0200

    Compiling for MacOS X with CMake

--- compile.sh
+++ compile.sh
@@ -2,30 +2,23 @@
 
 set -e
 
-if [[ ! -z $TRAVIS && $1 != "windows" ]]; then
-    # Install MonetDB compilation dependencies
-    apt-get -qq update && apt-get -qq -y install pkg-config pkgconf flex bison byacc
-    if [[ $1 == "macosx" ]] ; then
-        export OS=Darwin
-    else
-        export OS=Linux
-    fi
-fi
-
 case "$1" in
     windows)
         BUILDSYS=windows
-        BUILDLIBRARY=monetdb5.dll
+        BUILDINPUTLIBRARY=Release/monetdb5.dll
+        BUILDOUTPUTLIBRARY=monetdb5.dll
         ;;
 
     macosx)
         BUILDSYS=macosx
-        BUILDLIBRARY=libmonetdb5.dylib
+        BUILDINPUTLIBRARY=libmonetdb5.so
+        BUILDOUTPUTLIBRARY=libmonetdb5.dylib
         ;;
 
     *)
         BUILDSYS=linux
-        BUILDLIBRARY=libmonetdb5.so
+        BUILDINPUTLIBRARY=libmonetdb5.so
+        BUILDOUTPUTLIBRARY=libmonetdb5.so
 esac
 
 # Save the previous directory
@@ -35,13 +28,16 @@ cd $BASEDIR
 mkdir -p build/$BUILDSYS
 cd build/$BUILDSYS
 
-#Time to compile
+# Time to compile
 if [[ $1 == "windows" ]] ; then
     cmake -G "Visual Studio 15 2017 Win64" ../..
     cmake --build . --target ALL_BUILD --config Release
 else
-    echo `pwd`
     OPT=true cmake -G "Unix Makefiles" ../..
+    # For MacOS builds, change -Wl,-soname linker option
+    if [[ $1 == "macosx" ]] ; then
+        sed -i "s/-Wl,-soname,${BUILDINPUTLIBRARY}/-Wl,-install_name,${BUILDINPUTLIBRARY}/g" CMakeFiles/monetdb5.dir/link.txt
+    fi
     make clean && make -j
 fi
 
@@ -49,18 +45,15 @@ fi
 cd $BASEDIR
 mkdir -p monetdb-java-lite/src/main/resources/libs/$BUILDSYS
 
-if [[ $1 == "windows" ]] ; then
-	mv build/$BUILDSYS/Release/$BUILDLIBRARY monetdb-java-lite/src/main/resources/libs/$BUILDSYS/$BUILDLIBRARY
-else
-    mv build/$BUILDSYS/$BUILDLIBRARY monetdb-java-lite/src/main/resources/libs/$BUILDSYS/$BUILDLIBRARY
-fi
+mv build/$BUILDSYS/$BUILDINPUTLIBRARY monetdb-java-lite/src/main/resources/libs/$BUILDSYS/$BUILDOUTPUTLIBRARY
 
+# On Windows copy the runtime library
 if [[ $1 == "windows" ]] ; then
     cp -rf src/embeddedjava/windows/vcruntime140.dll monetdb-java-lite/src/main/resources/libs/$BUILDSYS/vcruntime140.dll
 fi
 
 # If we are not on Travis then we perform the gradle build
-if [ -z $TRAVIS ] ; then
+if [[ -z $TRAVIS ]] ; then
     cd monetdb-java-lite
     ./gradlew build
 else
