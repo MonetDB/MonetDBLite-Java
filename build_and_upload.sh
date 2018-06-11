0aee0641549a8145c421d5a3dcf5e1c259c3ae03
Pedro Ferreira <pedrotadim@outlook.com>
2018-06-11 16:41:52 +0200

    Cleaned build scripts

---
+++ build_and_upload.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -e
+
+function echo_and_exit {
+	echo $1 >&2
+	exit 1
+}
+
+if [[ -z $JAVA_HOME ]] ; then
+    echo_and_exit "The JAVA_HOME directory must be set"
+fi
+
+# Docker builds
+# docker run -it --rm -v $(pwd):/workdir:Z --privileged=true -e CROSS_TRIPLE=x86_64-linux-gnu -e NATIVE_LIBRARY_ONLY=1 pedrotadim/compilemonetdblite bash compile.sh linux Release
+# docker run -it --rm -v $(pwd):/workdir:Z --privileged=true -e CROSS_TRIPLE=x86_64-apple-darwin -e NATIVE_LIBRARY_ONLY=1 -e CROSS_COMPILING=1 pedrotadim/compilemonetdblite bash compile.sh macosx Release
+
+cd monetdb-java-lite
+
+./gradlew -i build
+./gradlew javadoc
+
+chmod -R 777 build
+
+# Prepare the directory to upload to our website
+mkdir -p synchronizing/javadocs/embedded
+# Move the header file for our Downloads section
+mv ../HEADER.html synchronizing
+# Move the javadocs
+mv build/docs/javadoc/* synchronizing/javadocs/embedded/
+# Download the monetdb-jdbc-new jar
+curl https://search.maven.org/remotecontent?filepath=monetdb/monetdb-jdbc-new/2.36/monetdb-jdbc-new-2.36.jar --output synchronizing/monetdb-jdbc-new-2.36.jar
+# Move the monetdb-java-lite jar
+mv build/libs/monetdb-java-lite-2.37.jar synchronizing
+# Rsync the library files to the monet.org machine
+rsync -aqz --ignore-times synchronizing/* ferreira@monetdb.org:/var/www/html/downloads/Java-Experimental/
+# Remove it in the end
+rm -rf synchronizing
+
+# Load our credentials
+MPASSWD=$(sed -n '2p' < gradle.properties | cut -d = -f 2)
+# printf -v MPASSWD "%s\n" $MPASSWD
+
+# Upload to Maven Central Repository
+yes $MPASSWD | head -100 | /opt/maven/bin/mvn deploy
--- compile.sh
+++ compile.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+function echo_and_exit {
+	echo $1 >&2
+	exit 1
+}
+
+if [[ "$#" != 2 ]]; then
+    echo_and_exit "Exactly 2 parameters must be given, OS and build type"
+fi
+
+if [[ ! "$1" =~ ^(windows|macosx|linux)$ ]]; then
+    echo_and_exit "The first parameter must either be 'windows', 'macosx' or 'linux'"
+fi
+
 case "$1" in
     windows)
         BUILDSYS=windows
@@ -21,19 +34,27 @@ case "$1" in
         BUILDOUTPUTLIBRARY=libmonetdb5.so
 esac
 
-# Save the previous directory
-PREVDIRECTORY=`pwd`
+if [[ ! "$2" =~ ^(Release|Debug)$ ]]; then
+    echo_and_exit "The second parameter must either be 'Release' or 'Debug'"
+fi
+
+# A bash 4.0 way of converting a string to lowercase...
+BUILDTYPE="${2,,}"
+
+echo "Performing ${BUILDTYPE} build on ${1}"
+
+# Move into the script's directory
 BASEDIR=$(realpath `dirname $0`)
 cd $BASEDIR
-mkdir -p build/$BUILDSYS
-cd build/$BUILDSYS
+mkdir -p build/$BUILDTYPE/$BUILDSYS
+cd build/$BUILDTYPE/$BUILDSYS
 
 # Time to compile
 if [[ $1 == "windows" ]] ; then
-    cmake -G "Visual Studio 15 2017 Win64" ../..
-    cmake --build . --target ALL_BUILD --config Release
+    cmake -G "Visual Studio 15 2017 Win64" ../../..
+    cmake --build . --target ALL_BUILD --config "$2"
 else
-    OPT=true cmake -G "Unix Makefiles" ../..
+    cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$2" ../../..
     # For MacOS builds, change -Wl,-soname linker option
     if [[ $1 == "macosx" ]] ; then
         sed -i "s/-Wl,-soname,${BUILDINPUTLIBRARY}/-Wl,-install_name,${BUILDINPUTLIBRARY}/g" CMakeFiles/monetdb5.dir/link.txt
@@ -45,20 +66,15 @@ fi
 cd $BASEDIR
 mkdir -p monetdb-java-lite/src/main/resources/libs/$BUILDSYS
 
-mv build/$BUILDSYS/$BUILDINPUTLIBRARY monetdb-java-lite/src/main/resources/libs/$BUILDSYS/$BUILDOUTPUTLIBRARY
+mv build/$BUILDTYPE/$BUILDSYS/$BUILDINPUTLIBRARY monetdb-java-lite/src/main/resources/libs/$BUILDSYS/$BUILDOUTPUTLIBRARY
 
 # On Windows copy the runtime library
 if [[ $1 == "windows" ]] ; then
     cp -rf src/embeddedjava/windows/vcruntime140.dll monetdb-java-lite/src/main/resources/libs/$BUILDSYS/vcruntime140.dll
 fi
 
-# If we are not on Travis then we perform the gradle build
-if [[ -z $TRAVIS ]] ; then
+# Sometimes is to desirable to compile the native library only
+if [[ -z $NATIVE_LIBRARY_ONLY ]] ; then
     cd monetdb-java-lite
     ./gradlew build
-else
-    # For when compiling in a Docker container
-    chmod -R 777 monetdb-java-lite
 fi
-
-cd $PREVDIRECTORY
