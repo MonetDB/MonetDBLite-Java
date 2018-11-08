c080115cbc698d5e8da6ebc9b7338993a9bbd52d
Pedro Ferreira <pedrotadim@outlook.com>
2018-11-08 15:21:59 +0100

    Added architecture compilation option and first support to Linux on arm64.

--- compile.sh
+++ compile.sh
@@ -7,22 +7,34 @@ function echo_and_exit {
 	exit 1
 }
 
-if [[ "$#" != 2 ]]; then
-    echo_and_exit "Exactly 2 parameters must be given, OS and build type"
+if [[ "$#" != 3 ]]; then
+    echo_and_exit "Exactly 3 parameters must be given, OS, architecture and build type"
 fi
 
 if [[ ! "$1" =~ ^(windows|macosx|linux)$ ]]; then
     echo_and_exit "The first parameter must either be 'windows', 'macosx' or 'linux'"
 fi
 
-if [[ ! "$2" =~ ^(Release|Debug)$ ]]; then
-    echo_and_exit "The second parameter must either be 'Release' or 'Debug'"
+if [[ ! "$2" =~ ^(amd64|x86_64|arm64|aarch64)$ ]]; then
+    echo_and_exit "The second parameter must either be 'arm64' or 'x86_64'"
+fi
+
+if [[ ! "$3" =~ ^(Release|Debug)$ ]]; then
+    echo_and_exit "The third parameter must either be 'Release' or 'Debug'"
+fi
+
+if [[ "$2" =~ ^(amd64|x86_64)$ ]]; then
+    ARCH_DIR="x86x64"
+elif [[ "$2" =~ ^(arm64|aarch64)$ ]]; then
+    ARCH_DIR="arm64"
+else
+    echo_and_exit "Unknown architecture"
 fi
 
 case "$1" in
     windows)
         BUILDSYS=windows
-        BUILDINPUTLIBRARY="$2"/monetdb5.dll
+        BUILDINPUTLIBRARY="$3"/monetdb5.dll
         BUILDOUTPUTLIBRARY=monetdb5.dll
         ;;
 
@@ -39,46 +51,46 @@ case "$1" in
 esac
 
 # A bash 4.0 way of converting a string to lowercase...
-BUILDTYPE="${2,,}"
+BUILDTYPE="${3,,}"
 
-echo "Performing ${BUILDTYPE} build on ${1}"
+echo "Performing ${BUILDTYPE} build on ${1} on ${2}"
 
 # Move into the script's directory
 BASEDIR=$(realpath `dirname $0`)
-cd $BASEDIR
+cd "$BASEDIR"
 
 # Set the proper versions
 ./versions.sh
 
-mkdir -p build/$BUILDTYPE/$BUILDSYS
-cd build/$BUILDTYPE/$BUILDSYS
+mkdir -p build/"$BUILDTYPE"/"$BUILDSYS"
+cd build/"$BUILDTYPE"/"$BUILDSYS"
 
 # Time to compile
-if [[ $1 == "windows" ]] ; then
+if [[ "$1" == "windows" ]] ; then
     cmake -G "Visual Studio 15 2017 Win64" ../../..
-    cmake --build . --target ALL_BUILD --config "$2"
+    cmake --build . --target ALL_BUILD --config "$3"
 else
-    cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$2" ../../..
+    cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$3" ../../..
     # For MacOS builds, change -Wl,-soname linker option
-    if [[ $1 == "macosx" ]] ; then
+    if [[ "$1" == "macosx" ]] ; then
         sed -i "s/-Wl,-soname,${BUILDINPUTLIBRARY}/-Wl,-install_name,${BUILDINPUTLIBRARY}/g" CMakeFiles/monetdb5.dir/link.txt
     fi
     make clean && make -j
 fi
 
 # Move the compiled library to the Gradle directory
-cd $BASEDIR
-mkdir -p monetdb-java-lite/src/main/resources/libs/$BUILDSYS
+cd "$BASEDIR"
+mkdir -p monetdb-java-lite/src/main/resources/libs/"$BUILDSYS"/"$ARCH_DIR"
 
-mv build/$BUILDTYPE/$BUILDSYS/$BUILDINPUTLIBRARY monetdb-java-lite/src/main/resources/libs/$BUILDSYS/$BUILDOUTPUTLIBRARY
+mv build/"$BUILDTYPE"/"$BUILDSYS"/"$BUILDINPUTLIBRARY" monetdb-java-lite/src/main/resources/libs/"$BUILDSYS"/"$ARCH_DIR"/"$BUILDOUTPUTLIBRARY"
 
 # On Windows copy the runtime library
-if [[ $1 == "windows" ]] ; then
-    cp -rf src/embeddedjava/windows/vcruntime140.dll monetdb-java-lite/src/main/resources/libs/$BUILDSYS/vcruntime140.dll
+if [[ "$1" == "windows" ]] ; then
+    cp -rf src/embeddedjava/windows/vcruntime140.dll monetdb-java-lite/src/main/resources/libs/"$BUILDSYS"/"$ARCH_DIR"/vcruntime140.dll
 fi
 
 # Sometimes is to desirable to compile the native library only
-if [[ -z $NATIVE_LIBRARY_ONLY ]] ; then
+if [[ -z "$NATIVE_LIBRARY_ONLY" ]] ; then
     cd monetdb-java-lite
     ./gradlew build
 fi
